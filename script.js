7ef1acdc866a344c2407c4d8783925cc7615a715
JustinAlger2 <162372954+JustinAlger2@users.noreply.github.com>
2024-10-23 12:57:07 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const projects = [
     {
         title: "My First Project",
-        url: "http://link.to.my.first.project.com",
+        url: "https://justinalger.huskisites.com/webm1001/program/Portfolio/part5/Portfolio/Portfolio.html",
         description: "My first project is the first project I ever made, and it's cool because it taught me the basics of programming."
     },
     {
@@ -16,23 +16,18 @@ const projects = [
     }
 ];
 
-const projectList = document.getElementById('project-list');
-
+const projectList = document.getElementById("project-list");
 projects.forEach(project => {
     const listItem = document.createElement('li');
     listItem.className = 'project';
-
     const title = document.createElement('h2');
     const link = document.createElement('a');
     link.href = project.url;
     link.textContent = project.title;
     title.appendChild(link);
-
     const description = document.createElement('p');
     description.textContent = project.description;
-
     listItem.appendChild(title);
     listItem.appendChild(description);
-
     projectList.appendChild(listItem);
 });
